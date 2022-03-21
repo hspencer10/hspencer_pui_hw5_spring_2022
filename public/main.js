f54e558ae8c5b3d7aa60cf0de0047396ffd1b645
Helena Spencer <helenaspencer@macbook-pro-2.myfiosgateway.com>
2022-03-20 20:22:01 -0400

    removing course popup working

--- public/main.js
+++ public/main.js
@@ -41,5 +41,37 @@ function drawEvent(view, start, end, title, location) {
     view.appendChild(newEvent);
 }
 
+// Removing Popup
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBTN");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+modal.style.display = "none";
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  closeModal();
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
   
   
